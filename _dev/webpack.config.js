e619e7b9e97399f2811c2d77d62759ed9f670970
Julien Bourdeau <julien@sigerr.org>
2016-08-09 15:06:50 +0200

    Rework Webpack conf to support ALL CSSpreprocessor

--- _dev/webpack.config.js
+++ _dev/webpack.config.js
@@ -1,35 +1,81 @@
 var webpack = require('webpack');
+var path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 var plugins = [];
 
 var production = false;
 
 if (production) {
-    plugins.push(
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        })
-    );
+  plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  );
 }
 
+plugins.push(
+  new ExtractTextPlugin(
+    path.join(
+      '..', 'css', 'theme.css'
+    )
+  )
+);
+
 module.exports = {
-    entry: [
-      './js/theme.js'
-    ],
-    output: {
-        path: '../assets/js',
-        filename: 'theme.js'
-    },
-    module: {
-        loaders: [
-            {test: /\.js$/     , loaders: ['babel-loader']},
-        ]
-    },
-    externals: {
-        prestashop: 'prestashop'
-    },
-    devtool: 'source-map',
-    plugins: plugins
+  entry: [
+    './css/_normalize.css',
+    './css/example.less',
+    './css/dev.styl',
+    './css/theme',
+    './js/theme.js'
+  ],
+  output: {
+    path: '../assets/js',
+    filename: 'theme.js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loaders: ['babel-loader']
+    }, {
+      test: /\.scss$/,
+      loader: ExtractTextPlugin.extract(
+        "style",
+        "css-loader?sourceMap!postcss!sass-loader?sourceMap"
+      )
+    }, {
+      test: /\.styl$/,
+      loader: ExtractTextPlugin.extract(
+        "style",
+        "css-loader?sourceMap!postcss!stylus-loader?sourceMap"
+      )
+    }, {
+      test: /\.less$/,
+      loader: ExtractTextPlugin.extract(
+        "style",
+        "css-loader?sourceMap!postcss!less-loader?sourceMap"
+      )
+    }, {
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract(
+        'style',
+        'css-loader?sourceMap!postcss-loader'
+      )
+    }, {
+      test: /.(png|woff(2)?|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/,
+      loader: 'file-loader?name=../css/[hash].[ext]'
+    }]
+  },
+  externals: {
+    prestashop: 'prestashop'
+  },
+  plugins: plugins,
+  resolve: {
+    extensions: ['', '.js', '.scss', '.styl', '.less', '.css']
+  }
 };
